869b1ee22bbc361444b079a6e1764035a54289b3
AvetisDN <avetisdn@gmail.com>
2024-12-25 19:32:10 +0300

    1

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getStrapiURL() {
-  return process.env.BACKEND_URL ?? "http://localhost:1337";
+  return process.env.STRAPI_URL ?? "http://localhost:1337";
 }
 
 export function getStrapiMedia(url: string | null) {
